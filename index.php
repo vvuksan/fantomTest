8dfdc79c1fcd5104aee8c5651d0a110fa6cf3bf9
Vladimir <vlemp@vuksan.com>
2013-01-31 10:58:38 -0500

    Add DNS and Ping MTR

--- index.php
+++ index.php
@@ -13,43 +13,85 @@ function getTimings() {
 	$("#results").html(data);
      });
 }
+function getDns() {
+    $("#dns_results").html('<img src="img/spinner.gif">');
+    $.get('get_dns.php', $("#dns_form").serialize(), function(data) {
+	$("#dns_results").html(data);
+     });
+}
+function getPingMtr() {
+    $("#pingmtr_results").html('<img src="img/spinner.gif">');
+    $.get('get_mtr.php', $("#pingmtr_form").serialize(), function(data) {
+	$("#pingmtr_results").html(data);
+     });
+}
 </script>
 </head>
 <body>
 <div id="tabs">
     <ul>
 	<li><a href="#tab-waterfall">URL Test</a></li>
+	<li><a href="#tab-dns">DNS</a></li>
+	<li><a href="#tab-pingmtr">Ping/MTR</a></li>
     </ul>
 
 <div id="tab-waterfall">
-<div id="large_screenshot">
+  <div id="large_screenshot">
+  </div>
+  <div id=header>
+  
+  <form id="query_form">
+  <?php
+  require_once('./conf.php');
+  if ( isset($conf['remotes']) and is_array($conf['remotes'] ) ) {
+      print "Test from <select name='site_id'><option value='-1'>Local</option>";
+      foreach ( $conf['remotes'] as $index => $remote ) {
+	print "<option value='" . $index . "'>" . $remote['name'] . "</option>"; 
+      }
+      print "</select> ";
+  }
+  ?>
+  URL <input id="checked_url" name="url" size=100>
+  <button class="query_buttons" id="query_button" onclick="getTimings(); return false;">Get timings</button>
+  <br />
+  <input type="checkbox" name="include_image">Include page screenshot<br>
+  </form>
+  </div>
+  <div id=results>
+  </div>
 </div>
-<div id=header>
 
-<form id="query_form">
-<?php
-require_once('./conf.php');
-if ( isset($conf['remotes']) and is_array($conf['remotes'] ) ) {
-    print "Test from <select name='site_id'><option value='-1'>Local</option>";
-    foreach ( $conf['remotes'] as $index => $remote ) {
-      print "<option value='" . $index . "'>" . $remote['name'] . "</option>"; 
-    }
-    print "</select> ";
-}
-?>
-URL <input id="checked_url" name="url" size=100>
-<button id="query_button" onclick="getTimings(); return false;">Get timings</button>
-<br />
-<input type="checkbox" name="include_image">Include page screenshot<br>
-</form>
+<div id="tab-dns">
+  <div id=header>
+  
+  <form id="dns_form">
+  Host name <input id="hostname" name="hostname" size=100>
+  <button class="query_buttons" id="dns_querybutton" onclick="getDns(); return false;">Resolve</button>
+  <br />
+  </form>
+  </div>
+  <div id=dns_results>
+  </div>
 </div>
-<div id=results>
+
+<div id="tab-pingmtr">
+  <div id=header>
+  
+  <form id="pingmtr_form">
+  Host name <input id="hostname" name="hostname" size=100>
+  <button class="query_buttons" id="dns_querybutton" onclick="getPingMtr(); return false;">Ping/MTR</button>
+  <br />
+  </form>
+  </div>
+  <div id=pingmtr_results>
+  </div>
 </div>
-</div>
+
+
 <script>
 $(function(){
     $("#tabs").tabs();
-    $("#query_button").button();
+    $(".query_buttons").button();
 
     $('#large_screenshot').dialog({
       title: "Large Picture",
