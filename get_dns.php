d541d3825032fcee3a39286e182a46bc41e0879c
Vladimir <vuksan-github@vuksan.com>
2013-03-12 13:38:05 -0400

    Support for remotes in DNS/MTR

--- get_dns.php
+++ get_dns.php
@@ -1,26 +1,54 @@
 <?php
 
-/* Request "ANY" record for php.net,
-   and create $authns and $addtl arrays
-   containing list of name servers and
-   any additional records which go with
-   them */
-$result = dns_get_record($_REQUEST['hostname'], DNS_A);
-#, DNS_ALL, $authns, $addtl);
-#print "<PRE>";
-#print_r($result);
+require_once("./conf.php");
 
-if ( count($result) > 0 ) {
-    print "<table border=1><tr><th>Hostname</th><th>TTL</th><th>Type</th><th>IP</th></tr>";
-    foreach( $result as $index => $record ) {
-        print "<tr><td>" . $record['host'] . "</td><td>" .
-            $record['ttl'] . "</td><td>" .
-            $record['type'] . "</td><td>" .
-            $record['ip'] . "</td></tr>";
-    }
-    print "</table>";
-} else {
-    
+$site_id = is_numeric($_REQUEST['site_id']) ? $_REQUEST['site_id'] : -1;
+
+if ( !isset($_REQUEST['hostname'])) {
+    die("Need to supply hostname");
 }
+
+if ( $_REQUEST['site_id'] == -1 ) {
+
+    $result = dns_get_record($_REQUEST['hostname'], DNS_A);
+    
+    if ( count($result) > 0 ) {
+        print "<table border=1><tr><th>Hostname</th><th>TTL</th><th>Type</th><th>IP</th></tr>";
+        foreach( $result as $index => $record ) {
+            print "<tr><td>" . $record['host'] . "</td><td>" .
+                $record['ttl'] . "</td><td>" .
+                $record['type'] . "</td><td>" .
+                $record['ip'] . "</td></tr>";
+        }
+        print "</table>";
+    } else {
+        
+    }
+
+} else if ( $site_id == -100 ) {
+
+    // Get results from all remotes         
+    foreach ( $conf['remotes'] as $index => $remote ) {
+        
+        print "<div><h3>" .$conf['remotes'][$index]['name']. "</h3></div>";
+        print "<div class=dns_results>";
+        print (file_get_contents($conf['remotes'][$index]['base_url'] . "get_dns.php?site_id=-1" .
+        "&hostname=" . $_REQUEST['hostname'] ));
+        print "</div>";
+    }
+
+} else if ( isset($conf['remotes'][$site_id]['name'] ) ) {
+    
+    print "<div><h3>" .$conf['remotes'][$site_id]['name']. "</h3></div>";
+    print "<div class=dns_results>";
+    print (file_get_contents($conf['remotes'][$site_id]['base_url'] . "get_dns.php?site_id=-1" .
+    "&hostname=" . $_REQUEST['hostname'] ));
+    print "</div>";
+    
+    
+} else {
+    die("No valid site_id supplied");
+}
+
 
 ?>
